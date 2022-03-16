c0206935b347808fbd10a5966b4b461d124c7e96
gnipka <gnipka29@gmail.com>
2022-03-16 17:45:13 +0300

    Добавлено логгирование

--- Microservices/Controllers/WeatherForecastController.cs
+++ Microservices/Controllers/WeatherForecastController.cs
@@ -15,6 +15,7 @@ namespace Microservices.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger, ValueHolder holder)
         {
             _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
             _holder = holder;
         }
 
@@ -22,7 +23,7 @@ namespace Microservices.Controllers
         public IActionResult Create([FromBody] WeatherForecast input)
         {
             _holder.Add(input);
-
+            _logger.LogInformation("Вызван метод создания объекта класса WeatherForecast");
             return Ok();
         }
 
@@ -32,10 +33,16 @@ namespace Microservices.Controllers
             var value = _holder.Values.FindLast(x => x.Date == input.Date);
 
             if (value is null)
+            {
                 _holder.Add(input);
+                _logger.LogInformation("Вызван метод обновления объекта класса WeatherForecast. Объект не был найден, поэтому создан новый");
+            }
 
             else
+            {
                 value.TemperatureC = input.TemperatureC;
+                _logger.LogInformation("Вызван метод обновления объекта класса WeatherForecast. Объект найден и обновлен");
+            }
 
             return Ok();
         }
@@ -46,9 +53,13 @@ namespace Microservices.Controllers
             var value = _holder.Values.FindLast(x => x.Date >= date1 && x.Date <= date2 || x.Date >= date2 && x.Date <= date1);
 
             if (value is null)
+            {
+                _logger.LogInformation("Вызван метод удаления объекта класса WeatherForecast. Объект не найден.");
                 return Problem($"Вы пытаетесь удалить не существующий объект. Погода с {date1} по {date2} не найдена");
+            }
 
             _holder.Delete(value);
+            _logger.LogInformation("Вызван метод удаления объекта класса WeatherForecast. Объект удален.");
             return Ok();
         }
 
@@ -56,12 +67,14 @@ namespace Microservices.Controllers
         [HttpGet, Route("readWithCondition")]
         public IEnumerable<WeatherForecast> Get([FromBody] DateTime date1, [FromQuery] DateTime date2)
         {
+            _logger.LogInformation("Вызван метод чтения экземпляров класса WeatherForecast с условием по дате. Информация возвращена пользователю.");
             return _holder.Values.FindAll(x => x.Date >= date1 && x.Date <= date2 || x.Date >= date2 && x.Date <= date1).ToArray();
         }
 
         [HttpGet, Route("read")]
         public IEnumerable<WeatherForecast> Read()
         {
+            _logger.LogInformation("Вызван метод чтения экземпляров класса WeatherForecast. Информация возвращена пользователю.");
             return _holder.Values.ToArray();
         }
     }
